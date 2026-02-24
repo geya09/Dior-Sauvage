896333bd65ca7240f37e47bb04c8cc55078d6b8d
geya09 <geaajengg@gmail.com>
2026-02-24 12:07:25 +0700

    innitial commit
    menambahkan script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// buy now button
+document.querySelector(".btn-primary").addEventListener("click", function () {
+
+    document.querySelector("#products").scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
+
+
+// NAVBAR PRODUCTS BUTTON
+document.querySelector(".products-btn").addEventListener("click", function () {
+    document.querySelector("#products").scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
+
+// NAVBAR REVIEW BUTTON
+document.querySelector(".about-btn").addEventListener("click", function () {
+
+document.querySelector("#about").scrollIntoView({
+behavior: "smooth"
+});
+
+});
